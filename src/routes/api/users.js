b7805cc49d91572d3c3cb73cb057c9038cca1c22
Tory Hebert <tory37@gmail.com>
2019-07-02 17:48:48 -0500

    removed remnant line

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -4,7 +4,6 @@ const express = require(`express`);
 const router = express.Router();
 const bcrypt = require(`bcryptjs`);
 const jwt = require(`jsonwebtoken`);
-const keys = require(`../../../config/keys`);
 var passport = require(`passport`);
 
 // Load input validation
