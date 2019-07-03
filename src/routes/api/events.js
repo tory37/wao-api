099f431f850733112d5005d90f92d0e0cccb2bc9
Tory Hebert <tory37@gmail.com>
2019-07-02 23:14:40 -0500

    setup event update

--- src/routes/api/events.js
+++ src/routes/api/events.js
@@ -7,7 +7,7 @@ const usersHelper = require(`../../utils/usersHelper`);
 var passport = require(`passport`);
 
 // Load input validation
-const isEventCreateInputValid = require(`../../validation/eventCreate`);
+const isEventObjectValid = require(`../../validation/eventObject`);
 // const isEventUpdateInputValid = require(`../../validation/eventUpdate`);
 
 // Load Event model
@@ -47,11 +47,10 @@ router.get(`/`, (req, res, next) => {
 // @access Private
 router.put(`/`, passport.authenticate(`jwt`, { session: false }), (req, res, next) => {
 	let errorObject = createErrorObject();
-	console.log(req.body);
 
 	try {
 		// Form validation
-		const isValid = isEventCreateInputValid(req.body, errorObject);
+		const isValid = isEventObjectValid(req.body, errorObject);
 
 		// Check validation
 		if (!isValid) {
@@ -73,6 +72,51 @@ router.put(`/`, passport.authenticate(`jwt`, { session: false }), (req, res, nex
 			newEvent
 				.save()
 				.then(user => res.json(newEvent))
+				.catch(err => {
+					addErrorMessages(errorObject, err.message);
+					return res.status(400).json(errorObject);
+				});
+		} else {
+			addErrorMessages(errorObject, `Unauthorized`);
+			return res.status(400).json(errorObject);
+		}
+	} catch (err) {
+		console.log(err);
+		addErrorMessages(errorObject, err.message);
+		return res.status(400).json(errorObject);
+	}
+});
+
+router.put(`/:id`, passport.authenticate(`jwt`, { session: false }), (req, res, next) => {
+	let errorObject = createErrorObject();
+
+	try {
+		// Form validation
+		const isValid = isEventObjectValid(req.body, errorObject);
+
+		// Check validation
+		if (!isValid) {
+			return res.status(400).json(errorObject);
+		}
+
+		if (usersHelper.isAdmin(req.user)) {
+			const filter = {
+				_id: req.params.id
+			};
+
+			const update = {
+				imageUrl: req.body.imageUrl,
+				startTimestamp: req.body.startTimestamp,
+				endTimestamp: req.body.endTimestamp,
+				title: req.body.title,
+				address: req.body.address,
+				lat: req.body.lat,
+				lng: req.body.lng,
+				description: req.body.description
+			};
+
+			Event.findOneAndUpdate(filter, update, { new: true })
+				.then(updatedEvent => res.json(updatedEvent))
 				.catch(err => {
 					addErrorMessages(errorObject, err.message);
 					return res.status(400).json(errorObject);
